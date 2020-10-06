05da2ba8f1cc07331c432e4eb54e8aa826a33f1e
Rajat Verma <rajatverma5885045@gmail.com>
2020-10-06 12:45:13 +0530

    basic app complete

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,70 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="small-container">
+    <h1>Employees</h1>
+
+    <employee-form @add-employee="addEmployee" />
+    <employee-table
+      :employees="employees"
+      @edit-employee="editEmployee"
+      @delete-employee="deleteEmployee"
+    />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import EmployeeTable from '@/components/EmployeeTable';
+import EmployeeForm from '@/components/EmployeeForm';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    EmployeeTable,
+    EmployeeForm,
+  },
+  data() {
+    return {
+      employees: [
+        {
+          id: 1,
+          name: 'Richard Hendricks',
+          email: 'richard@piedpiper.com',
+        },
+        {
+          id: 2,
+          name: 'Bertram Gilfoyle',
+          email: 'gilfoyle@piedpiper.com',
+        },
+        {
+          id: 3,
+          name: 'Dinesh Chugtai',
+          email: 'dinesh@piedpiper.com',
+        },
+      ],
+    };
+  },
+  methods: {
+    addEmployee(employee) {
+      const lastId =
+        this.employees.length > 0
+          ? this.employees[this.employees.length - 1].id
+          : 0;
+      const id = lastId + 1;
+      const newEmployee = { ...employee, id };
+
+      this.employees = [...this.employees, newEmployee];
+    },
+
+    editEmployee(id, updatedEmployee) {
+      this.employees = this.employees.map((employee) =>
+        employee.id === id ? updatedEmployee : employee
+      );
+    },
+
+    deleteEmployee(id) {
+      this.employees = this.employees.filter((employee) => employee.id !== id);
+    },
+  },
+};
 </script>
 
 <style>
